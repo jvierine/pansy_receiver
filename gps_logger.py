b6360b79d4d10552cb100d1e9e4ec31f7cb9f1e6
Juha Vierinen <jvierine@gmail.com>
2025-01-16 15:02:47 +0000

    added gpslogger

---
+++ gps_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import time
+
+tnow=time.time()
+
+logfname="logs/gpslog-%f.log"%(tnow)
+
+os.environ['LD_LIBRARY_PATH']="/home/radar/.syowa/lib"
+lfo=open(logfname,"w")
+while True:
+    os.system("rm -f /tmp/gpslock")
+    cmd="./test_gps 2>/dev/null |grep "GPS lock" 1> /tmp/gpslock"
+    os.system(cmd)
+    f=open("/tmp/gpslock")
+    l=f.readlines().strip()
+    tnow=time.time()
+    lfo.write("%f %s\n"%(tnow,l))
+    lfo.flush()
+    time.sleep(1)
+
+lfo.close()
