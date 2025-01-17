73c48f0d91f5c6a58e483ed8a4565a03af4d952e
Juha Vierinen <jvierine@gmail.com>
2025-01-17 12:38:43 +0000

    asd

--- find_mode_starts.py
+++ find_mode_starts.py
@@ -4,46 +4,74 @@ import matplotlib.pyplot as plt
 import digital_rf as drf
 import os
 import stuffr
+import time
 
-metadata_dir = "/media/archive/metadata/tx"
-os.system("mkdir -p %s"%(metadata_dir))
+# TBD. 
 
-subdirectory_cadence_seconds = 3600
-file_cadence_seconds = 60
-samples_per_second_numerator = 1000000
-samples_per_second_denominator = 1
-file_name = "m_tx"
+def update_tx_pulses():
+    metadata_dir = "/media/archive/metadata/tx"
 
-dmw = drf.DigitalMetadataWriter(
-    metadata_dir,
-    subdirectory_cadence_seconds,
-    file_cadence_seconds,
-    samples_per_second_numerator,
-    samples_per_second_denominator,
-    file_name,
-)
+    if os.path.exists(metadata_dir):
+        print("metadata directory exists. searching for last timestamp")
+        try:
+            dmr = drf.DigitalMetadataReader(metadata_dir)
+            db = dmr.get_bounds()
+            print(db)
+        except:
+            print("couldn't read metadata")
+            db=[-1,-1]
+    else:
+        os.system("mkdir -p %s"%(metadata_dir))
 
-# this is where the data is
-d=drf.DigitalRFReader("/media/archive/")
-# tx channel bounds
-b=d.get_bounds("ch007")
+    subdirectory_cadence_seconds = 3600
+    file_cadence_seconds = 60
+    samples_per_second_numerator = 1000000
+    samples_per_second_denominator = 1
+    file_name = "m_tx"
 
-dt=60000000
-n_windows = int(n.floor((b[1]-b[0])/dt))
-i0=b[0]
-for i in range(n_windows):
-    start_idx=pd.find_m_mode_start(d,
-                                   i0=i0+i*dt,
-                                   i1=i0+i*dt+dt,
-                                   ch="ch007", # channel 007 is the transmit sample
-                                   debug=False)
-    print("%s found %d pulses"%(stuffr.unix2datestr((i0+i*dt)/1e6),len(start_idx)))
+    dmw = drf.DigitalMetadataWriter(
+        metadata_dir,
+        subdirectory_cadence_seconds,
+        file_cadence_seconds,
+        samples_per_second_numerator,
+        samples_per_second_denominator,
+        file_name,
+    )
 
-    if len(start_idx)>0:
-        print("writing metadata")
-        data_dict={}
-        # let's use 1 as id of standard M-mode
-        mode_id=n.array(n.repeat(1,len(start_idx)),dtype=n.int32)
-        data_dict["id"]=mode_id
-        dmw.write(start_idx,data_dict)
+    # this is where the data is
+    d=drf.DigitalRFReader("/media/archive/")
+    # tx channel bounds
+    b=d.get_bounds("ch007")
+
+    dt=60000000
+    n_windows = int(n.floor((b[1]-b[0])/dt))
+
+    i0=b[0]
+    if db[1] != -1:
+        # start where we left off, instead of the start
+        i0=db[1]+10*1600
+    print("starting at %d"%(i0))
+
+    for i in range(n_windows):
+        # search for the start of a continuous 20 IPP sequence
+        start_idx=pd.find_m_mode_start(d,
+                                       i0=i0+i*dt,
+                                       i1=i0+i*dt+dt,
+                                       ch="ch007", # channel 007 is the transmit sample
+                                       debug=False)
+        print("%s found %d pulses"%(stuffr.unix2datestr((i0+i*dt)/1e6),20*len(start_idx)))
+
+        if len(start_idx)>0:
+            print("writing metadata")
+            data_dict={}
+            # let's use 1 as id of standard M-mode
+            mode_id=n.array(n.repeat(1,len(start_idx)),dtype=n.int32)
+            data_dict["id"]=mode_id
+            dmw.write(start_idx,data_dict)
     
+
+if __name__ == "__main__":
+    while True:
+        print("looking for new tx pulses")
+        update_tx_pulses()
+        time.sleep(10)
