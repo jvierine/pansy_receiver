f46b18488f85685a140afb30c3e042a72c1653c6
Juha Vierinen <jvierine@gmail.com>
2025-01-18 08:29:58 +0000

    added

--- quick_search_meteor.py
+++ quick_search_meteor.py
@@ -31,16 +31,18 @@ def meteor_search():
     db = dmr.get_bounds()
     print(db)
     b=d.get_bounds("ch000")
-    n_blocks=int(n.floor((db[1]-b[0])/(ipp*n_codes)))
+
+    start_idx=db[1]-1000000
+    n_blocks=int(n.floor((db[1]-start_idx)/(ipp*n_codes)))
     
     RTI = n.zeros([n_beam,n_codes,ipp],dtype=n.float32)
 
     N=20*1600
     for bi in range(n_blocks):
-        #b=d.get_bounds("ch000")
+
         
-        i0=bi*ipp*n_codes + b[0]
-        i1=bi*ipp*n_codes + b[0] + ipp*n_codes + ipp
+        i0=bi*ipp*n_codes + start_idx
+        i1=bi*ipp*n_codes + start_idx + ipp*n_codes + ipp
 
         if (i0 > b[0]) & (i1 < b[1]):
             data_dict = dmr.read(i0, i1, "id")
