6cc6e5268dbe716f30ebf7bb6d47a5a4ed514341
Juha Vierinen <jvierine@gmail.com>
2025-01-22 09:43:26 +0000

    asd

--- quick_overview.py
+++ quick_overview.py
@@ -13,29 +13,31 @@ import scipy.fftpack as fp
 d=drf.DigitalRFReader("/media/archive")
 b=d.get_bounds("ch000")
 i0=b[0]+10000000
-n_windows=4000
+n_windows=1800
 dt=int(n.floor((b[1]-i0)/n_windows))
+
+
+n_days=n.ceil((b[1]-i0)/(3600*1000000))
 
 window_len=1600
 
-S=n.zeros([n_windows,window_len],dtype=n.float64)
+for di in range(n_days):
+    S=n.zeros([n_windows,window_len],dtype=n.float64)
+    tv=[]
+    i00=i0+di*24*3600*1000000
+    for i in range(n_windows):
+        print(i)
+        read_idx=i00+dt*i
+        try:
+            z=d.read_vector_c81d(read_idx,window_len,"ch007")
+            za=n.abs(z)
+            za=n.convolve(za,n.repeat(1/8,8),mode="same")
+            S[i,:]=za
+        except:
+            print("can't read")
+        tv.append(n.datetime64(int(read_idx/1e6), 's'))
+        
 
-tv=[]
-for i in range(n_windows):
-    print(i)
-    read_idx=i0+dt*i
-    z=d.read_vector_c81d(read_idx,window_len,"ch007")
-    za=n.abs(z)
-    za=n.convolve(za,n.repeat(1/8,8),mode="same")
-    #plt.plot(za)
-    #plt.show()
-    print(z[0])
-    tv.append(n.datetime64(int(read_idx/1e6), 's'))
-    S[i,:]=za
-
-plt.pcolormesh(tv,n.arange(1600),S.T)
-plt.colorbar()
-plt.show()
-plt.pcolormesh(S)
-plt.colorbar()
-plt.show()
+    plt.pcolormesh(tv,n.arange(1600),S.T)
+    plt.colorbar()
+    plt.show()
