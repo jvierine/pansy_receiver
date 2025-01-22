947dfcfb740352135298db7a4dd3350cdc2e0e25
Juha Vierinen <jvierine@gmail.com>
2025-01-22 16:13:59 +0000

    asd

--- quick_search_meteor.py
+++ quick_search_meteor.py
@@ -119,10 +119,12 @@ def meteor_search():
                 max_snr=n.max(snr)
                 print(max_snr)
                 if max_snr > 4:
+                    plt.subplot(121)
                     plt.pcolormesh(tv,rds.rangev,10.0*n.log10(snr),vmin=0,vmax=20)
-                    plt.show()
+                    plt.subplot(122)
                     plt.pcolormesh(tv,rds.rangev,RTIV.T/1e3,cmap="turbo")
                     plt.colorbar()
+                    plt.tight_layout()
                     plt.show()
 
                 #plt.plot(z_tx.real)
