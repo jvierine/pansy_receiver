5c208cf77728d74b06435b0690cae29ae06d5167
juha <you@example.com>
2025-01-26 17:34:11 +0100

    asd

--- plot_mf.py
+++ plot_mf.py
@@ -100,7 +100,8 @@ dt=60000000
 d=drf.DigitalRFReader("/media/archive/")
 # tx channel bounds
 b=d.get_bounds("ch007")
-start_idx=b[0]#db_mf[1]-2*60*60*1000000
+#start_idx=b[0]#db_mf[1]-2*60*60*1000000
+start_idx=db_mf[1]-2*60*60*1000000
 #start_idx=db_mf[0]
 n_min=int(n.floor((db_mf[1]-start_idx)/dt))
 for i in range(n_min):
