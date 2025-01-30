4d8e27faa8833f5dc70264eb71df6fae4b46d77d
Juha Vierinen <jvierine@gmail.com>
2025-01-30 14:17:30 +0000

    asd

---
+++ cluster_mf.py
@@ -0,0 +1,104 @@
+
+import numpy as n
+import pansy_detect as pd
+import matplotlib.pyplot as plt
+import pansy_modes as pm
+import scipy.signal.windows as sw
+import scipy.constants as c
+import digital_rf as drf
+import os
+import stuffr
+import time
+import scipy.fftpack as fp
+
+def cluster(tx_idx,
+            rg,
+            dop,
+            snr,
+            min_det=5
+            ):
+    idx=n.argsort(snr)[::-1]
+    pairs=[]
+    # look for measurement pair that best fits this measurement
+    for i in idx:
+        dt = (tx_idx-tx_idx[i])/1e6
+        dr = rg-dop*dt
+        plt.plot(dt,dr,".")
+        plt.show()
+        
+        
+
+def read_mf_output(dm_mf,i0,i1,snr_threshold=7,tx_pwr_threshold=1e9):
+    txpa=n.array([],dtype=n.float32)
+    txidxa=n.array([],dtype=n.uint64)
+    rnga=n.array([],dtype=n.float32)
+    dopa=n.array([],dtype=n.float32)
+    snra=n.array([],dtype=n.float32)
+    beam=n.array([],dtype=n.int32)         
+    # read 20 pulse sequences   
+    try:
+        data_dict = dm_mf.read(i0, i1, ("tx_pwr","max_range","tx_idxs","max_dopvel","max_snr","beam_pos_idx"))
+    except:
+        import traceback
+        traceback.print_exc()
+    
+
+    if len(data_dict.keys()) == 0:
+        print("no data")
+    else:
+        for k in data_dict.keys():
+            data=data_dict[k]
+            
+            txp=data["tx_pwr"]
+#            print(txp)
+            # use this threshold for tx power. check that it is okay!!!
+            if n.min(txp) > tx_pwr_threshold:
+                txpa=n.concatenate((txpa,txp))
+                txidxa=n.concatenate((txidxa,data["tx_idxs"]))
+                rnga=n.concatenate((rnga,data["max_range"]))
+                dopa=n.concatenate((dopa,data["max_dopvel"]))
+                snra=n.concatenate((snra,data["max_snr"]))
+                beam=n.concatenate((beam,data["beam_pos_idx"]))            
+            else:
+                print("low txpower. skipping")
+
+        gidx=n.where(snra>snr_threshold)[0]
+        txpa=txpa[gidx]
+        txidxa=txidxa[gidx]
+        rnga=rnga[gidx]
+        dopa=dopa[gidx]
+        snra=snra[gidx]
+        beam=beam[gidx]
+    return(txpa,txidxa,rnga,dopa,snra,beam)
+
+mf_metadata_dir = "/media/archive/metadata/mf"
+dm_mf = drf.DigitalMetadataReader(mf_metadata_dir)
+db_mf = dm_mf.get_bounds()
+
+dt=10000000
+#n_min=int(n.floor((db_mf[1]-db_mf[0])/dt))
+d=drf.DigitalRFReader("/media/archive/")
+# tx channel bounds
+b=d.get_bounds("ch007")
+#start_idx=b[0]#db_mf[1]-2*60*60*1000000
+start_idx=dt*int(n.floor(db_mf[0]/dt))#-2*60*60*1000000
+#start_idx=db_mf[0]
+n_min=int(n.floor((db_mf[1]-start_idx)/dt))
+for i in range(n_min):
+    i0=start_idx+i*dt
+    i1=start_idx+i*dt+dt 
+    txpa,txidxa,rnga,dopa,snra,beam=read_mf_output(dm_mf,i0,i1)
+
+    gidx=n.where(n.abs(dopa)>3e3)[0]
+    txpa=txpa[gidx]
+    txidxa=txidxa[gidx]
+    rnga=rnga[gidx]
+    dopa=dopa[gidx]
+    snra=snra[gidx]
+    beam=beam[gidx]
+    if len(txpa)<5:
+        print("not enough data")
+        continue
+    
+    cluster_idx=cluster(txidxa,rnga,dopa,snra)
+    
