dc496b20132be688712205d3e1307c8c070f8103
Juha Vierinen <jvierine@gmail.com>
2025-01-31 15:09:01 +0000

    asd

--- cluster_mf.py
+++ cluster_mf.py
@@ -112,7 +112,7 @@ def cluster(tx_idx,
             
             if (len(n.intersect1d(used_idx,c[0])) == 0) and (len(n.intersect1d(used_idx,c[1])) == 0):
                 # at most 5*2*1.6e-3 apart to try merging
-                max_dt=5*2*1.6e-3
+                max_dt=5*3*1.6e-3
                 if n.abs(t1-t0)<max_dt:
                     try_idx=n.concatenate((c[0],c[1]))
                     print(try_idx)
@@ -150,7 +150,7 @@ def cluster(tx_idx,
 
             if (len(n.intersect1d(used_idx,c[0])) == 0) and (len(n.intersect1d(used_idx,c[1]))==0):
                 # at most 5*4*1.6e-3 apart to try merging
-                max_dt=5*4*1.6e-3
+                max_dt=5*5*1.6e-3
                 if n.abs(t1-t0)< max_dt:
                     try_idx=n.concatenate((c[0],c[1]))
                     print(try_idx)
@@ -184,7 +184,7 @@ def cluster(tx_idx,
 
             if (len(n.intersect1d(used_idx,c[0])) == 0) and (len(n.intersect1d(used_idx,c[1]))==0):
                 # at most 5*8*1.6e-3 apart to try merging
-                max_dt=5*8*1.6e-3
+                max_dt=5*16*1.6e-3
                 if n.abs(t1-t0)< max_dt:
                     try_idx=n.concatenate((c[0],c[1]))
                     print(try_idx)
