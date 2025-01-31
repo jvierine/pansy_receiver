dd0ebb3eed623e79e07ad9a163525cd95618feb4
Juha Vierinen <jvierine@gmail.com>
2025-01-31 15:10:11 +0000

    asd

--- cluster_mf.py
+++ cluster_mf.py
@@ -184,7 +184,7 @@ def cluster(tx_idx,
 
             if (len(n.intersect1d(used_idx,c[0])) == 0) and (len(n.intersect1d(used_idx,c[1]))==0):
                 # at most 5*8*1.6e-3 apart to try merging
-                max_dt=5*16*1.6e-3
+                max_dt=5*32*1.6e-3
                 if n.abs(t1-t0)< max_dt:
                     try_idx=n.concatenate((c[0],c[1]))
                     print(try_idx)
