143233c09ff4378736c92a9fcbab07797c0c6d7e
juha <you@example.com>
2025-02-04 21:22:03 +0100

    cleanup raw voltage without pmse mode

---
+++ remove_utls.py
@@ -0,0 +1,31 @@
+import numpy as n
+import digital_rf as drf
+import os
+import stuffr
+import time
+
+
+ # this is where the data is
+d=drf.DigitalRFReader("/media/archive/")
+# tx channel bounds
+b=d.get_bounds("ch007")
+
+
+# transmit metadata
+metadata_dir = "/media/archive/metadata/tx"
+dmr = drf.DigitalMetadataReader(metadata_dir)
+db = dmr.get_bounds()
+
+dt=1000000
+
+s0=int(n.max((n.floor(b[0]/dt),n.floor(db[0]/dt))))
+
+print(s0)
+s1=int(n.floor(db[1]/dt))
+
+for i in range(s0,s1):
+    #    print(i)
+    data_dict = dmr.read(i*dt-1600*20, i*dt+dt, "id")
+    print("%d n_ipp %d"%(i,len(data_dict.keys())))
+    #for k in data_dict.keys():
+     #   print(k)
