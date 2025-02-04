70b9e07cd6f34ae2024880fb631d7665dd053a55
juha <you@example.com>
2025-02-04 10:54:09 +0100

    asd

--- collect_detections.py
+++ collect_detections.py
@@ -8,25 +8,43 @@ det_md_dir = "/media/archive/metadata/detections"
 dm = drf.DigitalMetadataReader(det_md_dir)
 b = dm.get_bounds()
 
-data_dict = dm.read(b[0], b[1], ("xhat"))
+data_dict = dm.read(b[0], b[1], ("xhat","tx_idx"))
 
 tv=[]
 v0s=[]
 r0s=[]
+durs=[]
 for k in data_dict.keys():
     print(k)
     data=data_dict[k]
-    print(data)
+    xhat=data["xhat"]
+    dur=(n.max(data["tx_idx"])-n.min(data["tx_idx"]))/1e6
     r0=data[0]
     v0=data[1]
     t0=k
     tv.append(stuffr.unix2date(t0/1e6))
     v0s.append(v0)
     r0s.append(r0)
+    durs.append(dur)
+
+plt.scatter(tv,r0s,c=durs,cmap="turbo")
+plt.title("%d meteors"%(len(tv)))
+cb=plt.colorbar()
+cb.set_label("Duration (s)")
+plt.xlabel("Time (unix)")
+plt.show()
+
 
 plt.scatter(tv,r0s,c=v0s,vmin=-73,vmax=0,s=1,cmap="turbo")
 plt.title("%d meteors"%(len(tv)))
 cb=plt.colorbar()
 cb.set_label("Doppler (km/s)")
+plt.xlabel("Time (unix)")
+plt.show()
+
+plt.scatter(tv,v0s,c=r0s,vmin=80,vmax=140,s=1,cmap="turbo")
+plt.title("%d meteors"%(len(tv)))
+cb=plt.colorbar()
+cb.set_label("Height (km)")
 plt.xlabel("Time (unix)")
 plt.show()
