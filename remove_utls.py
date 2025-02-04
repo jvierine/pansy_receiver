ea843f95c00460fe6077e54fbc10782271d4cff6
juha <you@example.com>
2025-02-04 21:48:23 +0100

    cleanup raw voltage without pmse mode

--- remove_utls.py
+++ remove_utls.py
@@ -21,20 +21,22 @@ dt=1000000
 
 s0=int(n.max((n.floor(b[0]/dt),n.floor(db[0]/dt))))
 
-print(s0)
+#print(s0)
 s1=int(n.floor(db[1]/dt))
 
 for i in range(s0,s1):
     #    print(i)
     data_dict = dmr.read(i*dt-1600*20, i*dt+dt, "id")
-    print("%d n_ipp %d"%(i,len(data_dict.keys())))
+#    print("%d n_ipp %d"%(i,len(data_dict.keys())))
     n_ipp=len(data_dict.keys())
     if n_ipp == 0:
+        print("no ipps")
         fl=d._get_file_list(i*dt+10,i*dt+1000,1000000,3600,1000)
         for f in fl:
             for c in ch:
                 fname="%s/%s/%s"%("/media/archive/",c,f)
                 print(fname)
+                os.system("ls %s"%(fname))
     #os.system("ls /media/archive/ch000/")
     #for k in data_dict.keys():
      #   print(k)
