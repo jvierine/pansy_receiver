d23836ef47136c6e8c44f1e5ba8205365c76a8d2
juha <you@example.com>
2025-02-05 10:20:42 +0100

    added overview plot

--- pmse_plot.py
+++ pmse_plot.py
@@ -14,20 +14,25 @@ b = md.get_bounds()
 
 dt=15000000
 
-L=3*3600*1000000
+L=3600*1000000
 n_windows=int(n.floor(L/dt))
 
 rvec=n.arange(0,150)
 n_r=len(rvec)
 rti=n.zeros([n_windows,n_r],dtype=n.float32)
+n_avg=n.zeros([n_windows,n_r],dtype=n.float32)
+
 for i in range(n_windows):
+    print(i)
     i0=b[0]+i*dt
     i1=b[0]+i*dt+dt
     data_dict = md.read(i0, i1, ("tx_pwr","max_range","tx_idxs","max_dopvel","max_snr","beam_pos_idx"))
     for k in data_dict.keys():
         ridx=int(n.round(data_dict[k]["max_range"][0]))
-        print(ridx)
+#        print(ridx)
         rti[i,ridx]+=data_dict[k]["max_snr"][0]
+        n_avg[i,ridx]+=1.0
+rti=rti/(n_avg+1.0)
 plt.pcolormesh(rti,vmin=5,vmax=100)
 plt.colorbar()
 plt.show()
