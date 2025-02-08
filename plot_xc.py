22e50f93f5e2141ac29d752474e53cb0b73735ab
juha <you@example.com>
2025-02-08 22:55:28 +0100

    changes

--- plot_xc.py
+++ plot_xc.py
@@ -31,7 +31,11 @@ for bi in range(n_b):
         fvec=n.fft.fftshift(n.fft.fftfreq(n_fft,d=5*1600/1e6))[f0:f1]
         tvs.append(stuffr.unix2date(data_dict[k]["i0"]/1e6))
         for i in range(5):
+
             mean_pwr=n.sum(n.abs(data_dict[k]["xc_arr"][0:7,i,:,:]),axis=0)
+            plt.pcolormesh(10.0*n.log10(mean_pwr))
+            plt.colorbar()
+            plt.show()
             dop_idx=n.argmax(mean_pwr,axis=0)
             pprofs[i].append(n.max(mean_pwr,axis=0))
             dprofs[i].append(fvec[dop_idx])#n.max(n.sum(n.abs(data_dict[k]["xc_arr"][0:7,i,:,:]),axis=0),axis=0))
