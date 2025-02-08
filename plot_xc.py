94f997224a5f9fc314dd2848233204365772b021
juha <you@example.com>
2025-02-08 13:08:46 +0100

    asd

--- plot_xc.py
+++ plot_xc.py
@@ -17,11 +17,13 @@ for i in range(5):
     pprofs.append([])
 r0=0
 r1=1
+tvs=[]
 for bi in range(n_b):
     data_dict = dm.read(b[0]+bi*dt, b[0]+bi*dt+dt, ("xc_arr","i0","i1","r0","r1"))
     for k in data_dict.keys():
         r0=data_dict[k]["r0"]
         r1=data_dict[k]["r1"]
+        tvs.append([data_dict[k]["i0"],data_dict[k]["i1"]])
         for i in range(5):
             pprofs[i].append(n.max(n.sum(n.abs(data_dict[k]["xc_arr"][i,0:7,:,:]),axis=0),axis=0))
 pprofs=n.array(pprofs)
