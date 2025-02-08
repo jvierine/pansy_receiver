63c5d5a960216c66dcbd4d42caeee9fe8584ca7d
juha <you@example.com>
2025-02-08 12:57:33 +0100

    asd

--- plot_xc.py
+++ plot_xc.py
@@ -12,14 +12,20 @@ b = dm.get_bounds()
 
 dt=60*1000000
 n_b=int((b[1]-b[0])/dt)
-pprof0=[]
-pprof1=[]
-pprof2=[]
-pprof3=[]
-pprof4=[]
+pprofs=[]
+for i in range(5):
+    pprofs.append([])
 
 for bi in range(n_b):
     data_dict = dm.read(b[0]+bi*dt, b[0]+bi*dt+dt, ("xc_arr","i0","i1"))
     for k in data_dict.keys():
-        plt.plot(n.max(data_dict[k]["xc_arr"][0,0,:,:],axis=0))
-        plt.show()
+        for i in range(5):
+            pprofs[i].append(n.max(data_dict[k]["xc_arr"][i,0,:,:],axis=0))
+pprofs=n.array(pprofs)
+for i in range(5):
+    plt.pcolormesh(pprofs[i])
+    plt.colorbar()
+    plt.show()
+    
+
+
