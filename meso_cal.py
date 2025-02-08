a809175bc50cb17a57be45c1c795ffba709c3de6
juha <you@example.com>
2025-02-08 23:36:44 +0100

    use zenith echo as known direction

--- meso_cal.py
+++ meso_cal.py
@@ -39,12 +39,12 @@ for bi in range(n_b):
         # zenith beam
         xc=data_dict[k]["xc_arr"][:,0,:,:]
         mean_pwr=n.sum(n.abs(data_dict[k]["xc_arr"][0:7,0,:,:]),axis=0)
-        mi,mj=n.unravel_index(n.argmax(mean_pwr),shape=mean_pwr.shape)
+        #mi,mj=n.unravel_index(n.argmax(mean_pwr),shape=mean_pwr.shape)
         #noise_floor=n.median(mean_pwr)
-        #dcpwr=(mean_pwr[zidx,:]-noise_floor)/noise_floor
-        #gidx=n.where(dcpwr > 100)[0]
-        #if len(gidx)>0:
-        cals.append(xc[:,mi,mj])
+        dcpwr=(mean_pwr[zidx,:]-noise_floor)/noise_floor
+        gidx=n.where(dcpwr > 25)[0]
+        for gi in gidx:
+            cals.append(xc[:,zidx,gi])
 
 cals=n.array(cals)
 print(cals.shape)
