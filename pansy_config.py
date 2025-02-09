1fcd6e507d25cea57d2eca4b1b085150de6f9eae
juha <you@example.com>
2025-02-09 09:45:16 +0100

    asd

--- pansy_config.py
+++ pansy_config.py
@@ -1,3 +1,4 @@
+import numpy as n
 
 mf_metadata_dir="/media/analysis/metadata/mf"
 tx_metadata_dir="/media/analysis/metadata/tx"
@@ -12,3 +13,32 @@ raw_voltage_dir="/media/archive/"
 # antenna position
 lat=-69.010833
 lon=39.599722
+
+# parse antenna connections
+f=open("cfg/connections.txt","r")
+channel_ids=[]
+for l in f.readlines():
+    channel_ids.append(l.split(",")[0])
+f.close()
+# parse antenna positions
+f=open("cfg/antpos.csv","r")
+f.readline()
+antenna={}
+for l in f.readlines():
+    r=l.split(",")
+    serial=r[0]
+    name=r[1]
+    id=r[2]
+    ready=r[3]
+    x=float(r[4])
+    y=float(r[5])
+    z=float(r[6])
+    antenna[serial]={"serial":serial,"id":id,"name":name,"x":x,"y":y,"z":z,"ready":ready}
+
+
+if __name__ == "__main__":
+    print(channel_ids)
+    for cid in channel_ids:
+        print(cid)
+        if cid != "RFTX":
+            print(antenna[cid])
