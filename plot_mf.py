be9cb48880175bd7bd8098eca46f83cbfae1d48a
juha <you@example.com>
2025-02-10 10:57:42 +0100

    ASd

--- plot_mf.py
+++ plot_mf.py
@@ -11,91 +11,92 @@ import stuffr
 import time
 import scipy.fftpack as fp
 
-def cluster(tx_idx,
-            rg,
-            dop,
-            snr,
-            min_det=5
-            ):
-    idx=n.where(n.abs(dop) > 3e3)[0]
-    #tx_idx=tx_idx[gidx]
-    #rg=rg[gidx]
-    #dop=dop[gidx]
-    tv=tx_idx/1e6
-    #idx=n.arange(len(dop),dtype=n.int64)
-    meteor_idxs=[]
-    while len(idx) > min_det:
-        # try to add measurements to peak snr obs
-        i0=n.argmax(snr[idx])
-        this_idx=n.array([idx[i0]],dtype=n.int64)
-        t0=tv[idx[i0]]
+if False:
+    def cluster(tx_idx,
+                rg,
+                dop,
+                snr,
+                min_det=5
+                ):
+        idx=n.where(n.abs(dop) > 3e3)[0]
+        #tx_idx=tx_idx[gidx]
+        #rg=rg[gidx]
+        #dop=dop[gidx]
+        tv=tx_idx/1e6
+        #idx=n.arange(len(dop),dtype=n.int64)
+        meteor_idxs=[]
+        while len(idx) > min_det:
+            # try to add measurements to peak snr obs
+            i0=n.argmax(snr[idx])
+            this_idx=n.array([idx[i0]],dtype=n.int64)
+            t0=tv[idx[i0]]
 
 
-        n_m=len(this_idx)
-        # if >= 3 measurements
-        # r(t) = r0 + v*(t-t0) + 0.5*a*(t-t0)**2
-        # if less than 3 measurements
-        # r(t) = r0 + v*(t-t0)
-        if n_m==1:
-            # doppler migration corrected range residuals. these are good candidates
-            rg_resid=rg[idx] - (rg[idx[i0]]+(dop[idx[i0]])*(tv[idx]-t0)) + 1e6* (n.abs((tv[idx]-t0))>0.01) + 1e6* (n.abs((tv[idx]-t0))==0)
-            # find best measurement. when only one meas, look only nearby
-            i1=idx[n.argmin(rg_resid)]
+            n_m=len(this_idx)
+            # if >= 3 measurements
+            # r(t) = r0 + v*(t-t0) + 0.5*a*(t-t0)**2
+            # if less than 3 measurements
+            # r(t) = r0 + v*(t-t0)
+            if n_m==1:
+                # doppler migration corrected range residuals. these are good candidates
+                rg_resid=rg[idx] - (rg[idx[i0]]+(dop[idx[i0]])*(tv[idx]-t0)) + 1e6* (n.abs((tv[idx]-t0))>0.01) + 1e6* (n.abs((tv[idx]-t0))==0)
+                # find best measurement. when only one meas, look only nearby
+                i1=idx[n.argmin(rg_resid)]
 
 
 
 
-        gidx=idx[n.where(n.abs(rg_resid)<3e3 )[0]]
-        if len(gidx)>min_det:
-            plt.plot(tv[idx]-t0,rg_resid,".")
-            plt.show()
+            gidx=idx[n.where(n.abs(rg_resid)<3e3 )[0]]
+            if len(gidx)>min_det:
+                plt.plot(tv[idx]-t0,rg_resid,".")
+                plt.show()
 
-            meteor_idxs.append(gidx)
-        idx=n.setdiff1d(idx,gidx)
-    return(meteor_idxs)
+                meteor_idxs.append(gidx)
+            idx=n.setdiff1d(idx,gidx)
+        return(meteor_idxs)
 
-def read_mf_output(dm_mf,i0,i1,snr_threshold=7,tx_pwr_threshold=1e9):
-    txpa=n.array([],dtype=n.float32)
-    txidxa=n.array([],dtype=n.uint64)
-    rnga=n.array([],dtype=n.float32)
-    dopa=n.array([],dtype=n.float32)
-    snra=n.array([],dtype=n.float32)
-    beam=n.array([],dtype=n.int32)         
-    # read 20 pulse sequences   
-    try:
-        data_dict = dm_mf.read(i0, i1, ("tx_pwr","max_range","tx_idxs","max_dopvel","max_snr","beam_pos_idx"))
-    except:
-        import traceback
-        traceback.print_exc()
-    
+    def read_mf_output(dm_mf,i0,i1,snr_threshold=7,tx_pwr_threshold=1e9):
+        txpa=n.array([],dtype=n.float32)
+        txidxa=n.array([],dtype=n.uint64)
+        rnga=n.array([],dtype=n.float32)
+        dopa=n.array([],dtype=n.float32)
+        snra=n.array([],dtype=n.float32)
+        beam=n.array([],dtype=n.int32)         
+        # read 20 pulse sequences   
+        try:
+            data_dict = dm_mf.read(i0, i1, ("tx_pwr","max_range","tx_idxs","max_dopvel","max_snr","beam_pos_idx"))
+        except:
+            import traceback
+            traceback.print_exc()
+        
 
-    if len(data_dict.keys()) == 0:
-        print("no data")
-    else:
-        for k in data_dict.keys():
-            data=data_dict[k]
-            
-            txp=data["tx_pwr"]
-#            print(txp)
-            # use this threshold for tx power. check that it is okay!!!
-            if n.min(txp) > tx_pwr_threshold:
-                txpa=n.concatenate((txpa,txp))
-                txidxa=n.concatenate((txidxa,data["tx_idxs"]))
-                rnga=n.concatenate((rnga,data["max_range"]))
-                dopa=n.concatenate((dopa,data["max_dopvel"]))
-                snra=n.concatenate((snra,data["max_snr"]))
-                beam=n.concatenate((beam,data["beam_pos_idx"]))            
-            else:
-                print("low txpower. skipping")
+        if len(data_dict.keys()) == 0:
+            print("no data")
+        else:
+            for k in data_dict.keys():
+                data=data_dict[k]
+                
+                txp=data["tx_pwr"]
+    #            print(txp)
+                # use this threshold for tx power. check that it is okay!!!
+                if n.min(txp) > tx_pwr_threshold:
+                    txpa=n.concatenate((txpa,txp))
+                    txidxa=n.concatenate((txidxa,data["tx_idxs"]))
+                    rnga=n.concatenate((rnga,data["max_range"]))
+                    dopa=n.concatenate((dopa,data["max_dopvel"]))
+                    snra=n.concatenate((snra,data["max_snr"]))
+                    beam=n.concatenate((beam,data["beam_pos_idx"]))            
+                else:
+                    print("low txpower. skipping")
 
-        gidx=n.where(snra>snr_threshold)[0]
-        txpa=txpa[gidx]
-        txidxa=txidxa[gidx]
-        rnga=rnga[gidx]
-        dopa=dopa[gidx]
-        snra=snra[gidx]
-        beam=beam[gidx]
-    return(txpa,txidxa,rnga,dopa,snra,beam)
+            gidx=n.where(snra>snr_threshold)[0]
+            txpa=txpa[gidx]
+            txidxa=txidxa[gidx]
+            rnga=rnga[gidx]
+            dopa=dopa[gidx]
+            snra=snra[gidx]
+            beam=beam[gidx]
+        return(txpa,txidxa,rnga,dopa,snra,beam)
 
 mf_metadata_dir = "/media/archive/metadata/mf"
 dm_mf = drf.DigitalMetadataReader(mf_metadata_dir)
@@ -107,7 +108,7 @@ d=drf.DigitalRFReader("/media/archive/")
 # tx channel bounds
 b=d.get_bounds("ch007")
 #start_idx=b[0]#db_mf[1]-2*60*60*1000000
-start_idx=dt*int(n.floor(db_mf[0]/dt))#-2*60*60*1000000
+start_idx=dt*int(n.floor((db_mf[1]-300*1000000)/dt))#-2*60*60*1000000
 #start_idx=db_mf[0]
 n_min=int(n.floor((db_mf[1]-start_idx)/dt))
 for i in range(n_min):
