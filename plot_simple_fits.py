15012450cfa20aa87061e36a7d639bf7ba73f651
Juha Vierinen <jvierine@gmail.com>
2025-02-12 14:58:29 +0000

    added plot of simple fit

--- plot_simple_fits.py
+++ plot_simple_fits.py
@@ -4,7 +4,44 @@ import matplotlib.pyplot as plt
 import digital_rf as drf
 import stuffr
 
+import jcoord
+from astropy import units as u
+from astropy.coordinates import AltAz
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import EarthLocation
+import astropy.coordinates as ac
+from astropy.time import Time
+import numpy as n
 
+def get_radiant(p0,t0,u0):
+    """
+    initial position (enu)
+    time (unix)
+    u0 unit vector indication velocity vector of meteor (enu)
+    """
+    p0=jcoord.enu2ecef(mc.coords["lat"],mc.coords["lon"],0,-p0[1],-p0[0],p0[2])
+    u0=jcoord.enu2ecef(mc.coords["lat"],mc.coords["lon"],0,-u0[1],-u0[0],u0[2])
+    
+    # tbd: convert to astropy
+    llh=jcoord.ecef2geodetic(p0[0], p0[1], p0[2])
+    llh2=jcoord.ecef2geodetic(p0[0]-0.1*u0[0], p0[1]-0.1*u0[1], p0[2]-0.1*u0[2])
+    aar=jcoord.geodetic_to_az_el_r(llh[0],llh[1],llh[2], llh2[0], llh2[1], llh2[2])
+#    print(llh[2]/1e3)
+    
+    loc = EarthLocation(lat=llh[0]*u.deg, lon=llh[1]*u.deg, height=llh[2]*u.m)
+    obs_time=Time(t0,format="unix")
+    aa = AltAz(location=loc, obstime=obs_time)
+    sky = SkyCoord(alt = aar[1]*u.deg, az = aar[0]*u.deg, obstime = obs_time, frame = 'altaz', location = loc)
+
+    c_ecl=sky.transform_to('geocentricmeanecliptic')
+
+    sp=ac.get_sun(obs_time)
+    sun_pos=sp.transform_to('geocentricmeanecliptic')
+
+    sc_gc_lon = 180.0*n.angle(n.exp(-1j*n.pi*sun_pos.lon.deg/180.0)*n.exp(1j*n.pi*c_ecl.lon.deg/180.0))/n.pi
+    sc_gc_lat = c_ecl.lat.deg
+
+    return(sky,sc_gc_lat,sc_gc_lon, sun_pos.lon.deg)
 
 dm = drf.DigitalMetadataReader(pc.simple_fit_metadata_dir)
 b = dm.get_bounds()
@@ -31,6 +68,10 @@ for i in range(n_block):
         el=180*n.arctan(n.abs(v1[2])/v_h)/n.pi
         az=180*n.arccos(v1[1]/v_h)/n.pi
         
+        r,sc_lat,sc_lon,sun_lon=get_radiant(r0,k/1e6,v1/n.linalg.norm(v1))
+        print(sc_lat)
+        print(sc_lon)
+                
         std=data[k]["std"]
         if n.max(std*1e3) < 500.0:
             hgts.append(r0[2])
