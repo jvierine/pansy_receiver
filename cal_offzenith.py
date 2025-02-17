b62a5d288e029db6f81d02b8c16717f1ef71b044
juha <you@example.com>
2025-02-17 22:03:35 +0100

    off zenith calibration based on overlapping meteors

--- cal_offzenith.py
+++ cal_offzenith.py
@@ -12,7 +12,8 @@ def fit_beam_cal(xc0,xcn,ch_pairs):
     weight = goodness of xc
     ch_pairs = pairs of antenna channels cross-correlated
 
-    Assume that z_a^1  = z_a^0 e^{i\phi^1_a}.
+    Assume that z_a^1 e^{i\phi^1_a} is the correct signal. Here phi_a^1 is the phase correction for 
+    antenna a with pointing direciton 1.
     """
     # each antenna needs to be multiplied with these phases (n.exp(1j*phasecal))
     h=h5py.File("data/phases.h5","r")
@@ -23,28 +24,41 @@ def fit_beam_cal(xc0,xcn,ch_pairs):
         #phasecalz=n.exp(1j*(phasecal[ch_pairs[:,0]]-phasecal[ch_pairs[:,1]]))
         zenith_model = n.exp(1j*n.angle(xc0))*n.exp(1j*(phasecal[ch_pairs[:,0]]-phasecal[ch_pairs[:,1]]))
         #xprint(zenith_model)
-        offzenith_model=zenith_model*n.exp(1j*(phases[ch_pairs[:,0]]-phases[ch_pairs[:,1]]))
+        offzenith_model=n.exp(1j*n.angle(xcn))*n.exp(1j*(phases[ch_pairs[:,0]]-phases[ch_pairs[:,1]]))
         #print(offzenith_model)
-        res=n.array(offzenith_model-n.exp(1j*n.angle(xcn)),dtype=n.complex128)
+        res=n.array(offzenith_model-zenith_model,dtype=n.complex128)
         #print(res)
         s=n.sum(res.real**2.0 + res.imag**2.0)
-        #print(s)
         print(s)
         return(s)
     
     phases=so.fmin(ss,n.zeros(7,n.float32))
-    phases=so.fmin(ss,phases)#n.zeros(7,n.float32))
-    phases=so.fmin(ss,phases)#n.zeros(7,n.float32))
+    phases=so.fmin(ss,phases)
 
     zenith_model = n.exp(1j*n.angle(xc0))*n.exp(1j*(phasecal[ch_pairs[:,0]]-phasecal[ch_pairs[:,1]]))
-    offzenith_model=zenith_model*n.exp(1j*(phases[ch_pairs[:,0]]-phases[ch_pairs[:,1]]))
-    zd=n.angle(n.exp(1j*n.angle(xcn))*n.conj(offzenith_model))
-    for i in range(21):
-        plt.plot(zd[:,i],".")
-        plt.show()
-#    plt.plot(xc0.real,".")
- #   plt.plot(xc0.imag,".")
-  #  plt.show()
+    offzenith_model=n.exp(1j*n.angle(xcn))*n.exp(1j*(phases[ch_pairs[:,0]]-phases[ch_pairs[:,1]]))
+    zd=n.angle(zenith_model*n.conj(offzenith_model))
+
+    if False:
+        for i in range(2):
+            plt.plot(zd[:,i],".")
+            plt.show()
+    # remove outliers
+    gidx=n.where( (n.max(n.abs(zd[:,:]),axis=1)<1) )[0]
+    xc0=xc0[gidx,:]
+    xcn=xcn[gidx,:]
+    phases=so.fmin(ss,phases)
+    phases=so.fmin(ss,phases)
+
+    zenith_model = n.exp(1j*n.angle(xc0))*n.exp(1j*(phasecal[ch_pairs[:,0]]-phasecal[ch_pairs[:,1]]))
+    offzenith_model=n.exp(1j*n.angle(xcn))*n.exp(1j*(phases[ch_pairs[:,0]]-phases[ch_pairs[:,1]]))
+    zd=n.angle(zenith_model*n.conj(offzenith_model))
+    if False:
+        for i in range(21):
+            plt.plot(zd[:,i],".")
+            plt.show()
+
+    return(phases)
 
 h=h5py.File("caldata/cal_all.h5","r")
 
@@ -60,4 +74,7 @@ for i in range(1,5):
     xc0=h["beam%d/xc0"%(i)][()]
     xcn=h["beam%d/xcn"%(i)][()]
 
-    fit_beam_cal(xc0,xcn,ch_pairs)
+    phases=fit_beam_cal(xc0,xcn,ch_pairs)
+    ho=h5py.File("data/phases%d.h5"%(i),"w")
+    ho["phasecal"]=phases
+    ho.close()
