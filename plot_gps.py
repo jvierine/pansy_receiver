7baae40abff36a6fdfbaf062ee6df005fb9350bb
juha <you@example.com>
2025-02-21 00:40:07 +0100

    plot gps log

--- plot_gps.py
+++ plot_gps.py
@@ -2,7 +2,7 @@ import pansy_config as pc
 import numpy as n
 import matplotlib.pyplot as plt
 import digital_rf as drf
-
+import stuffr
 
 dm = drf.DigitalMetadataReader(pc.gpslock_metadata_dir)
 b = dm.get_bounds()
@@ -14,8 +14,11 @@ holdover=[]
 
 for k in dd.keys():
     holdover.append(dd[k]["holdover"])
-    tv.append(k/1e6)
+    tv.append(stuffr.unix2date(k/1e6))
 
 
 plt.plot(tv,holdover)
+plt.title("GPS log")
+plt.xlabel("Time (UT)")
+plt.ylabel("Holdover time (s)")
 plt.show()
