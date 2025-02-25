cdf6eccfcf44d0876285ed14bfc9240ad87c1bbb
Juha Vierinen <jvierine@gmail.com>
2025-02-25 16:05:10 +0000

    addeed b7

--- pansy_detect.py
+++ pansy_detect.py
@@ -261,7 +261,9 @@ def find_isr_mode_start(d,
 
         thresh=190e3
         prev_idx=0
+        print(ccm[mi])
         if ccm[mi]>thresh and (i0+mi - prev_idx) != 0:
+
             start_idxs.append(i0+mi)
             prev_idx=i0+mi
             z=d.read_vector_c81d(i0+mi,10000,ch)
