3a3d10cf1332cf9104bef192c17013d9fcdd8177
juha <you@example.com>
2025-02-27 21:59:37 +0100

    added mode detection for barker13

--- pansy_detect.py
+++ pansy_detect.py
@@ -309,6 +309,8 @@ def find_b13_mode_start(d,
         Z=fft.fft(z)
         cc=fft.ifft(Z*ZTX)
         ccm=cc.real**2.0+cc.imag**2.0
+        plt.plot(ccm)
+        plt.show()
         mi=n.argmax(ccm)
 
         print(ccm[mi])
