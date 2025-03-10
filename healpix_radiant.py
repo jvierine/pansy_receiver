10c18c1d912d9cbda516ed8088e6dfbfc66a27b0
juha <you@example.com>
2025-03-10 10:26:01 +0100

    added healpix vel

--- healpix_radiant.py
+++ healpix_radiant.py
@@ -2,13 +2,17 @@ import numpy as np
 import healpy as hp
 import matplotlib.pyplot as plt 
 import h5py 
+import numpy as n
 
 # Generate some random latitude and longitude points (or use real data)
 #num_points = 1000
 h=h5py.File("slon.h5","r")
 lons=h["slon"][()]
 lats=h["slat"][()]
+vg=h["vg"][()]
 h.close()
+print(vg)
+#exit(0)
 print(len(lons))
 #lats = np.random.uniform(-90, 90, num_points)  # Latitude in degrees
 #lons = np.random.uniform(-180, 180, num_points)  # Longitude in degrees
@@ -25,8 +29,22 @@ pixels = hp.ang2pix(nside, theta, phi)
 
 # Create a HEALPix map and count occurrences in each pixel
 histogram = np.bincount(pixels, minlength=hp.nside2npix(nside))
+mean_vel=n.zeros(len(histogram))
 
+for i in range(len(pixels)):
+    mean_vel[pixels[i]]+=vg[i]
+mean_vel=mean_vel/histogram
+mean_vel[histogram<5]=n.nan
+print(histogram.shape)
+histogram[histogram<5]=1
 # Plot the histogram as a HEALPix map
-hp.mollview(histogram, title="Histogram of radiants", unit="Counts",cmap="turbo",flip="geo")
+hp.mollview(histogram, title="Histogram of radiants", unit="Counts",cmap="viridis",flip="geo",norm="log")
+hp.graticule(color="white",alpha=0.1)
+plt.show()
+
+mean_vel[mean_vel<0]=0
+mean_vel[mean_vel>72]=72
+
+hp.mollview(mean_vel, title="Histogram of radiants", unit="Counts",cmap="turbo",flip="geo",norm="linear")
 hp.graticule(color="white",alpha=0.1)
 plt.show()
--- plot_avg_beam.py
+++ plot_avg_beam.py
@@ -6,40 +6,80 @@ import os
 import stuffr
 
 
-dm = drf.DigitalMetadataReader("/Users/j/src/pansy_test_data/metadata/simple_meteor_fit")
-b = dm.get_bounds()
-d=dm.read(b[0],b[1])
+def plot_blocks(block=3600*1000000):
+    dm = drf.DigitalMetadataReader("/Users/j/src/pansy_test_data/metadata/simple_meteor_fit")
+    b = dm.get_bounds()
 
-ew=[]
-ns=[]
-snr=[]
-mfs=[]
+    n_blocks=int(n.floor((b[1]-b[0])/block))
 
-for k in d.keys():
-    print(k)
-    vg=n.linalg.norm(d[k]["v0"])
-    print(vg)
-    if n.max(d[k]["std"]) < 400.0 and vg > 10: 
-        gidx=n.where(d[k]["mfs"]/21>0.9)[0]
-        ew=n.concatenate((ew,d[k]["ew"][gidx]))
-        ns=n.concatenate((ns,d[k]["ns"][gidx]))
-        snr=n.concatenate((snr,d[k]["snr"][gidx]))
-        mfs=n.concatenate((mfs,d[k]["mfs"][gidx]))
+    for bi in range(n_blocks):
+        ew=[]
+        ns=[]
+        up=[]
+        snr=[]
+        mfs=[]
+        d=dm.read(b[0]+bi*block,b[0]+bi*block+block)
 
-plt.subplot(121)
-#plt.scatter(ew,ns,c=10.0*n.log10(snr),s=1,vmin=10,vmax=20)
-plt.scatter(ew,ns,c=10.0*n.log10(snr),vmin=10,vmax=20,s=1,cmap="gist_yarg",alpha=0.1)#,vmin=10,vmax=20)
+        for k in d.keys():
+            print(k)
+  #          vg=n.linalg.norm(d[k]["v0"])
+ #           print(vg)
+#            if n.max(d[k]["std"]) < 400.0 and vg > 10: 
+            gidx=n.where(d[k]["mfs"]/21>0.1)[0]
+            ew=n.concatenate((ew,d[k]["ew"][gidx]))
+            ns=n.concatenate((ns,d[k]["ns"][gidx]))
+            up=n.concatenate((up,d[k]["up"][gidx]))
+            snr=n.concatenate((snr,d[k]["snr"][gidx]))
+            mfs=n.concatenate((mfs,d[k]["mfs"][gidx]))
+        if len(ew)>0:
+            ew=n.array(ew)
+            ns=n.array(ns)
+            up=n.array(up)
+            rng=n.sqrt(ew**2.0+ns**2.0+up**2.0)
+            u=ew/rng
+            v=ns/rng
+     #       plt.hist2d(ns,up,bins=[n.linspace(-30,30,num=100),n.linspace(75,135,num=100)],weights=mfs)
+    #        plt.title("Histogram of detections")
+   #         plt.xlabel("North-South (km)")
+  #          plt.ylabel("Up (km)")
+ #           plt.tight_layout()
+#            plt.show()
 
-plt.xlabel("East-West (km)")
-plt.ylabel("North-South (km)")
-#cb=plt.colorbar()
-#cb.set_label("SNR (dB)")
-#plt.show()
-plt.subplot(122)
 
-plt.hist2d(ew,ns,bins=[n.linspace(-30,30,num=250),n.linspace(-30,30,num=250)],weights=mfs)
-plt.title("Histogram of detections")
-plt.xlabel("East-West (km)")
-plt.ylabel("North-South (km)")
-plt.tight_layout()
-plt.show()
+    #        plt.subplot(121)
+            #plt.scatter(ew,ns,c=10.0*n.log10(snr),s=1,vmin=10,vmax=20)
+            plt.figure(figsize=(16,8))
+            plt.subplot(121)
+            plt.scatter(u,v,c=mfs/21,vmin=0,vmax=1,s=1,cmap="viridis",alpha=1.0)#,vmin=10,vmax=20)
+            plt.colorbar()
+            plt.xlim([-0.3,0.3])
+            plt.ylim([-0.3,0.3])
+
+#            plt.xlim([-30,30])
+ #           plt.ylim([-30,30])
+            plt.title(stuffr.unix2datestr((b[0]+bi*block)/1e6))
+            plt.xlabel("East-West (km)")
+            plt.ylabel("North-South (km)")
+            plt.tight_layout()
+            plt.subplot(122)
+#            plt.gca().set_aspect('equal') 
+            plt.scatter(ns,up,c=mfs/21,vmin=0,vmax=1,s=1,cmap="viridis",alpha=1.0)#,vmin=10,vmax=20)
+            plt.colorbar()
+            plt.xlim([-30,30])
+            plt.ylim([75,135])
+#            plt.title(stuffr.unix2datestr((b[0]+bi*block)/1e6))
+            plt.xlabel("North-South (km)")
+            plt.ylabel("Up (km)")
+            plt.tight_layout()
+            plt.gca().set_aspect('equal') 
+            plt.savefig("/tmp/uanim-%d.png"%(b[0]+bi*block))
+            plt.close()
+            plt.show()
+ #           plt.show()
+
+        #cb=plt.colorbar()
+        #cb.set_label("SNR (dB)")
+        #plt.show()
+ #       plt.subplot(122)
+
+plot_blocks()
