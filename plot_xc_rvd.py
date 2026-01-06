e5b06f86e9f2282b0ed32fc82b4bb05fc25a4157
Juha Vierinen <jvierine@gmail.com>
2026-01-06 13:15:13 +0000

    asd

--- plot_xc_rvd.py
+++ plot_xc_rvd.py
@@ -23,50 +23,35 @@ def plot_pprof(t0,t1):
     wprofs=[]
     data_dict = dm.read(t0, t1, ("i0","r0","r1","rvec","fvec"))
     n_b=0
-
+    fmax=20.0
+    keys=[]
     for k in data_dict.keys():
+        keys.append(k)
         print(k)
         rvec=data_dict[k]["rvec"]
         print(n.min(rvec))
         print(n.max(rvec))
         n_b+=1
         
-    exit(0)
+    n_r=len(rvec)
+    S=n.zeros([n_b,n_r],dtype=n.float32)
     for bi in range(n_b):
-        data_dict = dm.read(start_idx+bi*dt, start_idx+bi*dt+dt, ("xc_arr","i0","i1","r0","r1","f0","f1","n_fft","rdec"))
+        print(bi)
+        data_dict = dm.read(keys[bi]-10, keys[bi]+10, ("xc_arr","i0","i1","rvec","fvec"))
         for k in data_dict.keys():
-            r0=data_dict[k]["r0"]
-            r1=data_dict[k]["r1"]
-            f0=data_dict[k]["f0"]
-            f1=data_dict[k]["f1"]
-            rdec=data_dict[k]["rdec"]        
-            n_fft=data_dict[k]["n_fft"]
-            fvec=n.fft.fftshift(n.fft.fftfreq(n_fft,d=5*1600/1e6))[f0:f1]
-            tvs.append(stuffr.unix2date(data_dict[k]["i0"]/1e6))
-            for i in range(5):
-                mean_pwr=n.sum(n.abs(data_dict[k]["xc_arr"][0:7,i,:,:]),axis=0)
-                dop_idx=n.argmax(mean_pwr,axis=0)
-                pprofs[i].append(n.max(mean_pwr,axis=0))
-                dprofs[i].append(fvec[dop_idx])
-    pprofs=n.array(pprofs)
-    dprofs=n.array(dprofs)
-    rvec=n.arange(1600)*0.15
-    rvec=rvec[r0:r1:rdec]
-#    fig, axs = plt.subplots(nrows=5,ncols=1)
-#    for i in range(5):
- #       ax=axs[i]    
-  #      if i == 0:
-    i=0
-    dB=10.0*n.log10(pprofs[i,:,:].T)
-    nfloor=n.nanmedian(dB)
-    dB=dB-nfloor
-    m=ax.pcolormesh(tvs,rvec,dB,vmin=0)
-    ax.set_xlabel("Date (UTC)")
-    ax.set_ylabel("Range (km)")
-    fig.autofmt_xdate()
-#    cb=fig.colorbar(m,ax=ax)
- #   cb.set_label("SNR (dB)")
+            rvec=data_dict[k]["rvec"]
+            xc=data_dict[k]["xc_arr"]
+            fvec=data_dict[k]["fvec"]        
+            fidx=n.where(n.abs(fvec)<fmax)[0]
+            mean_pwr=n.sum(n.abs(data_dict[k]["xc_arr"][0:7,0,:,:]),axis=0)
+            noise_floor=n.median(mean_pwr)
+            snr=(mean_pwr-noise_floor)/noise_floor
+            snr_prof=n.sum(mean_pwr[fidx,:],axis=0)
+            S[bi,:]=snr_prof
+    plt.pcolormesh(10.0*n.log10(S))
+    plt.colorbar()
+    plt.show()
 
 
 tnow=time.time()
-plot_pprof(int(tnow*1e6-24*3600*1e6),int(tnow*1e6))
+plot_pprof(int(tnow*1e6-1*3600*1e6),int(tnow*1e6))
